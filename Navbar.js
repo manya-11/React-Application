cab85857f6b3feac3bf27efe6044855f6e10bcff
manya salwan <122377214+manya-11@users.noreply.github.com>
2024-05-20 14:24:06 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar({onSearch}) {
+    const [query,setQuery] = useState("")
+  return (
+    <div className='navbar'>
+        <h1>MOVIES</h1>
+        <ul className='nav'>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/movies">Movie</Link>
+            </li>
+            <li>
+                <Link to="/tv">Tv</Link>
+            </li>
+            <li>
+                <Link to="/favMov">Movie Library</Link>
+            </li>
+            <li>
+                <Link to="/favTv">Tv Library</Link>
+            </li>
+            <li>
+                <Link to="/Signin">Signin</Link>
+            </li>
+        </ul>
+        <div className='search'>
+            <input type="text" placeholder='Search' onChange={(e)=>setQuery(e.target.value)}/>
+            <button onClick={()=>onSearch(query)}>Search</button>
+        </div>
+    </div>
+  )
+}
